2af43d3cce7914c41bc639bec190fdc67c9a11f8
juliely8619 <july8619@yahoo.com>
2019-01-10 11:20:28 -0800

    added loop and ifs

--- Challenges/BinarySearch/BinarySearch/Program.cs
+++ Challenges/BinarySearch/BinarySearch/Program.cs
@@ -13,9 +13,30 @@ namespace BinarySearch
             BinarySearch(array2, 5); //one that returns -1
         }
 
-        public static int BinarySearch(int[] array, int key)
+        public static int BinarySearch(int[] arr, int key)
         {
-            return 1;
+            int start = 0; //for now I assume my array will always start at 0
+            int end = arr.Length - 1;
+            int midVal = (start + end) / 2;
+            do
+            {
+                if (arr[midVal] == key)
+                {
+                    return key;
+                }
+                else if (arr[midVal] > key) //key is in bottom half of search range
+                {
+                    end = midVal - 1;
+                }
+                else if (arr[midVal] < key) // key is in top half of search range
+                {
+                    start = midVal + 1;
+                }
+                else
+                {
+                    return -1;
+                }
+            } while ((end - start) > 0);
         }
     }
 }
--- Challenges/XUnitTestProject1/UnitTest1.cs
+++ Challenges/XUnitTestProject1/UnitTest1.cs
@@ -18,5 +18,11 @@ namespace XUnitTestProject1
             int[] arr2 = new int[] {2,4,6,8};
             Assert.NotEqual(10, Program.BinarySearch(arr2, 3));
         }
+        [Fact]
+        public void TestBinarySearchReturnsOne()
+        {
+            int[] arr3 = new int[] {2,4,6,8};
+            Assert.Equal(-1, Program.BinarySearch(arr3, 3));
+        }
     }
 }
