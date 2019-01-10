b43815d6182fbbf9b796f4c8bf10abba41ef8803
juliely8619 <july8619@yahoo.com>
2019-01-10 11:12:57 -0800

    tests are working, need to update code to return correct information now

--- Challenges/BinarySearch/BinarySearch/Program.cs
+++ Challenges/BinarySearch/BinarySearch/Program.cs
@@ -2,17 +2,20 @@
 
 namespace BinarySearch
 {
-    Public class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            int[] array1 = new int[] { };
-            BinarySearch(array1, 10);
+            int[] array1 = new int[] {1,2,3,4};
+            BinarySearch(array1, 4); //one that works
+
+            int[] array2 = new int[] { 2,4,6,8};
+            BinarySearch(array2, 5); //one that returns -1
         }
 
-        private static int BinarySearch(int[] array, int key);
+        public static int BinarySearch(int[] array, int key)
         {
-            return -1;
+            return 1;
         }
     }
 }
--- Challenges/XUnitTestProject1/UnitTest1.cs
+++ Challenges/XUnitTestProject1/UnitTest1.cs
@@ -7,9 +7,16 @@ namespace XUnitTestProject1
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestBinarySearchWorks()
         {
-            Assert.Equal(,)
+            int[] arr1 = new int[] { 1, 2, 3, 4 };
+            Assert.Equal(1, Program.BinarySearch(arr1,3));
+        }
+        [Fact]
+        public void TestBinarySearchNotWorks()
+        {
+            int[] arr2 = new int[] {2,4,6,8};
+            Assert.NotEqual(10, Program.BinarySearch(arr2, 3));
         }
     }
 }
