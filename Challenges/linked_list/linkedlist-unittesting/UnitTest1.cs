8edca173b9ec88b4ddadba9b8300881d37860d4c
juliely8619 <july8619@yahoo.com>
2019-01-14 11:03:05 -0800

    all tests done

--- Challenges/linked_list/linkedlist-unittesting/UnitTest1.cs
+++ Challenges/linked_list/linkedlist-unittesting/UnitTest1.cs
@@ -8,26 +8,26 @@ namespace linkedlist_unittesting
     public class UnitTest1
     {
         //test include
-        //[Fact]
-        //public void TestInclude()
-        //{
-        //    LList list = new LList();
-        //    list.Insert(4);
-        //    list.Insert(8);
-        //    list.Insert(15);
+        [Fact]
+        public void TestInclude()
+        {
+            LList list = new LList();
+            list.Insert(4);
+            list.Insert(8);
+            list.Insert(15);
 
-        //    Assert.True(list.Includes(8));
-        //}
-        //[Fact]
-        //public void TestNotInclude()
-        //{
-        //    LList list = new LList();
-        //    list.Insert(4);
-        //    list.Insert(8);
-        //    list.Insert(15);
+            Assert.True(list.Includes(8));
+        }
+        [Fact]
+        public void TestNotInclude()
+        {
+            LList list = new LList();
+            list.Insert(4);
+            list.Insert(8);
+            list.Insert(15);
 
-        //    Assert.False(list.Includes(29));
-        //}
+            Assert.False(list.Includes(29));
+        }
         [Fact]
         public void TestInclude2()
         {
@@ -35,21 +35,21 @@ namespace linkedlist_unittesting
             list.Insert(4);
             list.Insert(8);
             list.Insert(15);
-
-            Assert.True(list.Includes(15));
+            bool temp = list.Includes(15);
+            Assert.True(temp);
         }
 
         //test insert
-        //[Fact]
-        //public void TestInsert()
-        //{
-        //    LList list = new LList();
-        //    list.Insert(4);
-        //    list.Insert(8);
-        //    list.Insert(15);
+        [Fact]
+        public void TestInsert()
+        {
+            LList list = new LList();
+            list.Insert(4);
+            list.Insert(8);
+            list.Insert(15);
 
-        //    Assert.True(list.Includes(4)); //because if it can find it, it was inserted
-        //}
+            Assert.True(list.Includes(4)); //because if it can find it, it was inserted
+        }
         [Fact]
         public void TestInsert2()
         {
@@ -60,16 +60,16 @@ namespace linkedlist_unittesting
 
             Assert.True(list.Includes(8)); //because if it can find it, it was inserted
         }
-        //[Fact]
-        //public void TestNotInsert() //i don't think this is how I should have tested it wasn't insterted...because I didn't try to insert it
-        //{
-        //    LList list = new LList();
-        //    list.Insert(4);
-        //    list.Insert(8);
-        //    list.Insert(15);
+        [Fact]
+        public void TestNotInsert() //i don't think this is how I should have tested it wasn't insterted...because I didn't try to insert it
+        {
+            LList list = new LList();
+            list.Insert(4);
+            list.Insert(8);
+            list.Insert(15);
 
-        //    Assert.False(list.Includes(29));
-        //}
+            Assert.False(list.Includes(29));
+        }
 
         //print method didn't return anthing so can't test it
 
