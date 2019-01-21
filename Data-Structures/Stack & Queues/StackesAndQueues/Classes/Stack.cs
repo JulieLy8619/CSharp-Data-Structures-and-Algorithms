8ef2dec49c1d4beec2d30156ebd04541ee242162
juliely8619 <july8619@yahoo.com>
2019-01-20 16:30:45 -0800

    push pop peek of stack

--- Data-Structures/Stack & Queues/StackesAndQueues/Classes/Stack.cs
+++ Data-Structures/Stack & Queues/StackesAndQueues/Classes/Stack.cs
@@ -6,5 +6,50 @@ namespace StackesAndQueues.Classes
 {
     class Stack
     {
+        public Node Top { get; set; }
+        
+        //stack created without a node given
+        public Stack()
+        {
+            Top = null;
+        }
+
+        //stack created with a node given
+        public Stack(Node node)
+        {
+            Top = node;
+        }
+
+        /// <summary>
+        /// adds a node to the stack
+        /// </summary>
+        /// <param name="value">value of the new node</param>
+        public void Push(int value)
+        {
+            Node newNode = new Node(value);
+            newNode.Next = Top;
+            Top = newNode;
+        }
+
+        /// <summary>
+        /// remove a node from the stack
+        /// </summary>
+        /// <returns>the top node</returns>
+        public Node Pop()
+        {
+            Node tempNode = Top;
+            Top = Top.Next;
+            tempNode.Next = null;
+            return tempNode;
+        }
+
+        /// <summary>
+        /// looks at what is on the top of the stack
+        /// </summary>
+        /// <returns>the information of the top node in the stack</returns>
+        public Node Peek()
+        {
+            return Top;
+        }
     }
 }
