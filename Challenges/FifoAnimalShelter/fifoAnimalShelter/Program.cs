c12a303e80cf4d1bdb75dd721e1d2d88ba59a73c
juliely8619 <july8619@yahoo.com>
2019-01-26 00:15:37 -0800

    added to program

--- Challenges/FifoAnimalShelter/fifoAnimalShelter/Program.cs
+++ Challenges/FifoAnimalShelter/fifoAnimalShelter/Program.cs
@@ -6,7 +6,16 @@ namespace fifoAnimalShelter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FIFOAnimalShelter();
+            //Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// a method that outputs a message (created soloy because the instructions stated so)
+        /// </summary>
+        public static void FIFOAnimalShelter()
+        {
+            Console.WriteLine("Hello World from FIFOAnimalShelter");
         }
     }
 }
