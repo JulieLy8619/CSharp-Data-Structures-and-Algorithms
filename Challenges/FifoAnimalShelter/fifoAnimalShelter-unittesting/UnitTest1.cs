18db1f9579d0014595d39ba5a9e7cbd8804f1d21
juliely8619 <july8619@yahoo.com>
2019-01-26 00:11:16 -0800

    added testing

---
+++ Challenges/FifoAnimalShelter/fifoAnimalShelter-unittesting/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace fifoAnimalShelter_unittesting
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
