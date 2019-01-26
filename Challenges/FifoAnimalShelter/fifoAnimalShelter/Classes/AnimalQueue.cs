c3863c8c385cb0e60f73e2d24d41880f6451907a
juliely8619 <july8619@yahoo.com>
2019-01-26 12:39:46 -0800

    added anumal queue

---
+++ Challenges/FifoAnimalShelter/fifoAnimalShelter/Classes/AnimalQueue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fifoAnimalShelter.Classes
+{
+    class AnimalQueue
+    {
+        //properties
+        public AnimalNode Front { get; set; }
+        public AnimalNode Rear { get; set; }
+
+        //instantiate (with and without starter "node")
+        public AnimalQueue()
+        {
+            Front = null;
+            Rear = null;
+        }
+        public AnimalQueue(AnimalNode node)
+        {
+            Front = node;
+            Rear = node;
+        }
+
+        //enqueue
+        public void Enqueue(Animal value)
+        {
+            if (Front == null) //if queue was empty and I add one
+            {
+                AnimalNode newNode = new AnimalNode(value);
+                Rear = newNode;//is this redundent because this should have happened in the instatiate with value because of line above
+                Front = newNode;//is this redundent
+            }
+            else
+            {
+                AnimalNode newNode = new AnimalNode(value);
+                Rear.Next = newNode;
+                Rear = newNode;
+            }
+        }
+
+        //dequeue
+        public AnimalNode Dequeue()
+        {
+            //should add check if trying to dequeue from an empty queue
+            if (Front == null)
+            {
+                return null;
+            }
+            else
+            {
+                AnimalNode temp = Front;
+                Front = Front.Next;
+                temp.Next = null;
+                return temp;
+            }
+        }
+
+        public AnimalNode Peek()
+        {
+            return Front;
+        }
+    }
+}
--- Challenges/FifoAnimalShelter/fifoAnimalShelter/Classes/AnimalShelter.cs
+++ Challenges/FifoAnimalShelter/fifoAnimalShelter/Classes/AnimalShelter.cs
@@ -4,14 +4,26 @@ using System.Text;
 
 namespace fifoAnimalShelter.Classes
 {
+    //should be like queue but with animal tye instad of int
     class AnimalShelter
     {
         //properties
+        public AnimalQueue AnimalQueue1 = new AnimalQueue();
+        public AnimalQueue AnimalQueue2 = new AnimalQueue();
 
         //instantiate (with and without starter "node")
+        public AnimalShelter()
+        {
+
+        }
+        public AnimalShelter(Animal newAnimal)
+        {
+            AnimalQueue1.Enqueue(newAnimal);
+        }
 
         //enqueue
 
-        //dequeue
+
+        //dequeue, this is where there is a param to check adopter's pref
     }
 }
