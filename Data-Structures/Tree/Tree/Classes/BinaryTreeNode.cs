b0a8c92cb63de1471c0981c98b4dee026d04c94e
juliely8619 <july8619@yahoo.com>
2019-01-27 13:54:44 -0800

    binary node done

---
+++ Data-Structures/Tree/Tree/Classes/BinaryTreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree.Classes
+{
+    class BinaryTreeNode
+    {
+        public int Value { get; set; }
+        public BinaryTreeNode LeftChild { get; set; }
+        public BinaryTreeNode RightChild { get; set; }
+
+        public BinaryTreeNode(int value)
+        {
+            Value = value;
+            LeftChild = null;
+            RightChild = null;
+        }
+    }
+}
