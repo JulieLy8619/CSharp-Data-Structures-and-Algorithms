b0c20730e53ed7087ba4f11a79be752bf679d1bb
juliely8619 <july8619@yahoo.com>
2019-01-26 16:36:01 -0800

    program done

--- Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -8,11 +8,25 @@ namespace MultiBracketValidation
         static void Main(string[] args)
         {
             Console.WriteLine("Should Pass: {}[([{}])]");
-            MultiBracketValidation("{}[([{}])]");
+            bool answer1 = MultiBracketValidation("{}[([{}])]");
+            Console.WriteLine(answer1);
+            Console.WriteLine(); //just because I like space
+
+            Console.WriteLine("Should Pass: ");
+            bool answer2 = MultiBracketValidation("(wordswordswords)");
+            Console.WriteLine(answer2);
             Console.WriteLine(); //just because I like space
 
             Console.WriteLine("Should Fail: (cat]");
-            MultiBracketValidation("(cat]");
+            bool answer3 = MultiBracketValidation("(cat]");
+            Console.WriteLine(answer3);
+            Console.WriteLine(); //just because I like space
+
+            Console.WriteLine("Should Fail: {(})");
+            bool answer4 = MultiBracketValidation("{(})");
+            Console.WriteLine(answer4);
+            Console.WriteLine(); //just because I like space
+
             Console.ReadLine(); //to stop it from auto exit
         }
 
@@ -23,7 +37,55 @@ namespace MultiBracketValidation
         /// <returns></returns>
         public static bool MultiBracketValidation(string input)
         {
-            
+            CharStack bracketStack = new CharStack();
+            for (int i = 0; i <input.Length; i++)
+            {
+                if (input[i] == '{' || input[i] == '[' || input[i] == '(')
+                {
+                    bracketStack.Push(input[i]);
+                }
+                if (input[i] == '}')
+                {
+                    if (bracketStack.Top != null && bracketStack.Top.Value == '{')
+                    {
+                        bracketStack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else if (input[i] == ']')
+                {
+                    if (bracketStack.Top != null && bracketStack.Top.Value == '[')
+                    {
+                        bracketStack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else if (input[i] == ')')
+                {
+                    if (bracketStack.Top != null && bracketStack.Top.Value == '(')
+                    {
+                        bracketStack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            if(bracketStack.Top == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
