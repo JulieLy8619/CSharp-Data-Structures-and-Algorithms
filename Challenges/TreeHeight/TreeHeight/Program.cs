9b1aea6a07bf8ff93601bea2d373d77931f29f42
juliely8619 <july8619@yahoo.com>
2019-02-11 11:38:56 -0800

    coded up height of tree

---
+++ Challenges/TreeHeight/TreeHeight/Program.cs
@@ -0,0 +1,40 @@
+﻿using BreadthFirstTraversal.Classes;
+using System;
+
+namespace TreeHeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static int FindTreeHeight(TreeNode root)
+        {
+            QueueForTrees methodQ = new QueueForTrees();
+            int heightAnswer = 0;
+
+            methodQ.Enqueue(root);
+
+            while (methodQ.Front != null)
+            {
+                if (methodQ.Front.Value.LeftChild != null || methodQ.Front.Value.RightChild != null)
+                {
+                    heightAnswer++;
+                }
+                Node tempNode = methodQ.Dequeue();
+                if (tempNode.Value.LeftChild != null)
+                {
+                    methodQ.Enqueue(tempNode.Value.LeftChild);
+                }
+                if (tempNode.Value.RightChild != null)
+                {
+                    methodQ.Enqueue(tempNode.Value.RightChild)
+                }
+            }
+            return heightAnswer;
+
+        }
+    }
+}
