58c847213b6e88f87828c61df5fd15ce9b521259
juliely8619 <july8619@yahoo.com>
2019-02-11 14:31:14 -0800

    created second challenge, is the ancestor in the tree

---
+++ Challenges/AncestorInTree/AncestoryInTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AncestoryInTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
