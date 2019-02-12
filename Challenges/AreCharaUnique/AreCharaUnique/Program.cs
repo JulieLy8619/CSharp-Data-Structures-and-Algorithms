496aad66725db9b7ca4453df08d14c11e8d8df1b
juliely8619 <july8619@yahoo.com>
2019-02-11 17:11:00 -0800

    done with if string is unique characters

--- Challenges/AreCharaUnique/AreCharaUnique/Program.cs
+++ Challenges/AreCharaUnique/AreCharaUnique/Program.cs
@@ -6,7 +6,45 @@ namespace AreCharaUnique
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool mainAnswer1 = AreCharactersUnique("tes");
+            Console.WriteLine("tes is " + mainAnswer1);
+            bool mainAnswer2 = AreCharactersUnique("test");
+            Console.WriteLine("test is " + mainAnswer2);
+            bool mainAnswer3 = AreCharactersUnique("`~234g");
+            Console.WriteLine("`~234g is " + mainAnswer3);
+
+            Console.ReadLine(); // so it doesn't auto quit
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool AreCharactersUnique(string input)
+        {
+            if (input.Length > 0)
+            {
+                string shortenString = input;
+                for (int i = 1; i < input.Length; i++)
+                {
+                    if (input[0] == input[i])
+                    {
+                        return false;
+                    }
+                }
+                if (input != null)
+                {
+
+                    shortenString = input.TrimStart(input[0]);
+                }
+                bool answer = AreCharactersUnique(shortenString);
+                return answer;
+            }
+            else
+            {
+                return true; //technically it is neither if it is null
+            }
         }
     }
 }
---
+++ Challenges/AreCharaUnique/AreCharaUnique/README.md
@@ -0,0 +1,17 @@
+## Find if a string has only unique characters
+We were asked to determine if a given string contained only unique characters
+
+## Challenge
+
+
+## Approach & Efficiency
+For time, 
+For space, 
+
+## Solution
+WhiteBoard: 
+![](../../../assets/)
+Console: 
+![](../../../assets/)
+Tests:
+![](../../../assets/)
