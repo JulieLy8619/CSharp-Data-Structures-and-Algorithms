62ca18dcc8d7ba201de498fe48b78b564a465e85
juliely8619 <july8619@yahoo.com>
2019-02-11 16:11:36 -0800

    done rewriting with breadth

--- Challenges/AncestorInTree/AncestoryInTree/Program.cs
+++ Challenges/AncestorInTree/AncestoryInTree/Program.cs
@@ -12,75 +12,47 @@ namespace AncestoryInTree
             TreeNode treeNode1 = new TreeNode(8);
             treeNode1.LeftChild = new TreeNode(5);
             treeNode1.RightChild = new TreeNode(6);
-            //treeNode1.RightChild.LeftChild = new TreeNode(1);
-            //treeNode1.RightChild.RightChild = new TreeNode(3);
+            treeNode1.RightChild.LeftChild = new TreeNode(1);
+            treeNode1.RightChild.RightChild = new TreeNode(3);
             Console.WriteLine("root value: " + treeNode1.Value);
             Console.WriteLine("Left child value: " + treeNode1.LeftChild.Value);
             Console.WriteLine("Right child value: " + treeNode1.RightChild.Value);
-            //Console.WriteLine("Right child's Left value: " + treeNode1.RightChild.LeftChild.Value);
-            //Console.WriteLine("Right child's Right value: " + treeNode1.RightChild.RightChild.Value);
-            bool answer = IsAncestor(8, 5, treeNode1);
-            Console.WriteLine("8 is an ancestor of 5 " + answer);
+            Console.WriteLine("Right child's Left value: " + treeNode1.RightChild.LeftChild.Value);
+            Console.WriteLine("Right child's Right value: " + treeNode1.RightChild.RightChild.Value);
+            bool answer = IsAncestor(8, 3, treeNode1);
+            Console.WriteLine("8 is an ancestor of 3 " + answer);
             Console.ReadLine(); // to stop it from auto quitting
         }
 
         public static bool IsAncestor(int A, int B, TreeNode root)
         {
-            //bool iaAnswer = false;
-            if (root == null)
+            QueueForTrees methodQ = new QueueForTrees();
+            bool foundA = false;
+
+            methodQ.Enqueue(root);
+            while (methodQ.Front != null)
             {
-                return false;
-            }
-            //B isn't even in the tree
-            if (TreeSearch(root, B) == false)
-            {
-                return false;
-            }
-            //A isn't even in the tree
-            if (TreeSearch(root, A) == false)
-            {
-                return false; 
-            }
-            //A is in the tree
-            else
-            {
-                if (root.Value == A)
+                Node tempNode = methodQ.Dequeue();
+                if (tempNode.Value.Value == A)
                 {
-                    return TreeSearch(root, B);
+                    foundA = true;
                 }
-                return false;
+                if (tempNode.Value.Value == B && foundA == true)
+                {
+                    return true;
+                }
+                if (tempNode.Value.LeftChild != null)
+                {
+                    methodQ.Enqueue(tempNode.Value.LeftChild);
+                }
+                if (tempNode.Value.RightChild != null)
+                {
+                    methodQ.Enqueue(tempNode.Value.RightChild);
+                }
             }
+            return false;
         }
 
-        public static bool TreeSearch(TreeNode root, int value, bool answer)
-        {
-            //bool answer = false;
-            if (root.Value == value)
-            {
-                //bool newAnswer = true;
-                answer = true;
-                return answer;
-            }
-            if (root == null)
-            {
-                return answer;
-            }
-            bool lAnswer = TreeSearch(root.LeftChild, value, answer);
-            bool rAnswer = TreeSearch(root.RightChild, value, answer);
-            return answer;
-            //if (root.LeftChild != null)
-            //{
-            //    TreeSearch(root.LeftChild, value);
-            //}
-            //if (root.RightChild != null)
-            //{
-            //    TreeSearch(root.RightChild, value);
-            //}
-            //else if (root.LeftChild == null && root.RightChild == null && root.Value != value)
-            //{
-            //    return false;
-            //}
-            
-        }
+        
     }
 }
