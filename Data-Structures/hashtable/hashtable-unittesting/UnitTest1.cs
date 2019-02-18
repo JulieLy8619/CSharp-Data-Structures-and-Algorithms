7d798f12e7c1c949c83280a77432079d7dfe1e7e
juliely8619 <july8619@yahoo.com>
2019-02-18 10:02:06 -0800

    tests are done

--- Data-Structures/hashtable/hashtable-unittesting/UnitTest1.cs
+++ Data-Structures/hashtable/hashtable-unittesting/UnitTest1.cs
@@ -148,28 +148,86 @@ namespace hashtable_unittesting
         //test Hashtable=============================
         //Adding a key/value to your hashtable results in the value being in the data structure
         [Fact]
-        public void TestHashTableAdd()
+        public void TestHashTableAdd1()
         {
             Hashtable testHashTable1 = new Hashtable();
             testHashTable1.AddToHashTable("cat", "dog");
             Assert.Equal("cat", testHashTable1.HashTableArray[3].Head.Key);
         }
+        [Fact]
+        public void TestHashTableAdd2()
+        {
+            Hashtable testHashTable2 = new Hashtable();
+            testHashTable2.AddToHashTable("cat", "dog");
+            Assert.Equal("dog", testHashTable2.HashTableArray[3].Head.Value);
+        }
 
         //Retrieving based on a key returns the value stored
+        [Fact]
+        public void TestHashTableGet()
+        {
+            Hashtable testHashTable3 = new Hashtable();
+            testHashTable3.AddToHashTable("cat", "dog");
+            string testAnswer1 = testHashTable3.GetFromHashTable("cat");
+            Assert.Equal("dog", testAnswer1);
+        }
 
         //Successfully returns null for a key that does not exist in the hashtable
+        [Fact]
+        public void TestHashTableGetForNull()
+        {
+            Hashtable testHashTable4 = new Hashtable();
+            testHashTable4.AddToHashTable("cat", "dog");
+            string testAnswer2 = testHashTable4.GetFromHashTable("bob");
+            Assert.Null(testAnswer2);
+        }
 
         //Successfully handle a collision within the hashtable
+        [Fact]
+        public void TestHashTableCollision1()
+        {
+            Hashtable testHashTable5 = new Hashtable();
+            testHashTable5.AddToHashTable("cat", "dog");
+            testHashTable5.AddToHashTable("rat", "newValue");
+            string testAnswer3 = testHashTable5.GetFromHashTable("rat");
+            Assert.Equal("newValue", testAnswer3);
+        }
+        [Fact]
+        public void TestHashTableCollision2()
+        {
+            Hashtable testHashTable6 = new Hashtable();
+            testHashTable6.AddToHashTable("cat", "dog");
+            testHashTable6.AddToHashTable("rat", "newValue");
+            string testAnswer4 = testHashTable6.GetFromHashTable("cat"); //checking orig is still there too
+            Assert.Equal("dog", testAnswer4);
+        }
 
         //Successfully retrieve a value from a bucket within the hashtable that has a collision
+        //I don't think I either did the above tests right or this one, because they are the same....
+        [Fact]
+        public void TestHashTableCollisionValue()
+        {
+            Hashtable testHashTable7 = new Hashtable();
+            testHashTable7.AddToHashTable("cat", "dog");
+            testHashTable7.AddToHashTable("rat", "newValue");
+            string testAnswer4 = testHashTable7.GetFromHashTable("rat");
+            Assert.Equal("newValue", testAnswer4);
+        }
 
         //Successfully hash a key to an in-range value
         [Fact]
-        public void TestHashIndexNumber()
+        public void TestHashIndexNumber1()
         {
             Hashtable testHashTable1 = new Hashtable();
             int htAnswer1 = testHashTable1.Hash("a");
             Assert.Equal(1, htAnswer1);
+        }
+        [Fact]
+        public void TestHashIndexNumber2()
+        {
+            Hashtable testHashTable11 = new Hashtable();
+            int htAnswer11 = testHashTable11.Hash("abcdef");
+            Assert.Equal(1, htAnswer11);
         }
 
 
