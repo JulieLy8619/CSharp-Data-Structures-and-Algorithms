9d88c85e03409ec8fdaace6c1bfe55190ecfe6d4
juliely8619 <july8619@yahoo.com>
2019-02-17 20:10:08 -0800

    made tests for node class (i don't think I HAD to but it is good practice)

--- Data-Structures/hashtable/hashtable-unittesting/UnitTest1.cs
+++ Data-Structures/hashtable/hashtable-unittesting/UnitTest1.cs
@@ -1,3 +1,4 @@
+using HashTable.Classes;
 using System;
 using Xunit;
 
@@ -5,11 +6,67 @@ namespace hashtable_unittesting
 {
     public class UnitTest1
     {
+        //test KVNodes==============================
+        //get key
         [Fact]
-        public void Test1()
+        public void TestGetKVNodeKey()
         {
-
+            KVNode testNode1 = new KVNode("cat", "dog");
+            Assert.Equal("cat", testNode1.Key);
         }
+
+        //set key
+        [Fact]
+        public void TestSetKVNodeKey()
+        {
+            KVNode testNode2 = new KVNode("cat", "dog");
+            testNode2.Key = "NewKey";
+            Assert.Equal("NewKey", testNode2.Key);
+        }
+
+        //get value
+        [Fact]
+        public void TestGetKVNodeValue()
+        {
+            KVNode testNode3 = new KVNode("cat", "dog");
+            Assert.Equal("dog", testNode3.Value);
+        }
+
+        //set value
+        [Fact]
+        public void TestSetKVNodeValue()
+        {
+            KVNode testNode4 = new KVNode("cat", "dog");
+            testNode4.Value = "NewValue";
+            Assert.Equal("NewValue", testNode4.Value);
+        }
+
+        //get next
+        //I don't know if this was the right way to do get for next because I assigned it...
+        [Fact]
+        public void TestGetKVNodeNext()
+        {
+            KVNode testNode5 = new KVNode("cat", "dog");
+            KVNode testNode6 = new KVNode("red", "blue");
+            testNode5.Next = testNode6;
+            Assert.Equal(testNode6, testNode5.Next);
+        }
+
+        //set next
+        [Fact]
+        public void TestSetKVNodeNext()
+        {
+            KVNode testNode7 = new KVNode("cat", "dog");
+            KVNode testNode8 = new KVNode("red", "blue");
+            KVNode testNode9 = new KVNode("apple", "banana");
+            testNode7.Next = testNode8;
+            testNode7.Next = testNode9;
+            Assert.Equal(testNode9, testNode7.Next);
+        }
+
+        //test KVNodeLinkList========================
+
+        //test Hashtable=============================
     }
 }
 
