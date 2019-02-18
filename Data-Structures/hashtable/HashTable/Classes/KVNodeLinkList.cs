af895c50ab2dd5d05f22249ed9dd700744a88040
juliely8619 <july8619@yahoo.com>
2019-02-17 19:35:24 -0800

    i think the revised linklist is written

--- Data-Structures/hashtable/HashTable/Classes/KVNodeLinkList.cs
+++ Data-Structures/hashtable/HashTable/Classes/KVNodeLinkList.cs
@@ -6,5 +6,55 @@ namespace HashTable.Classes
 {
     public class KVNodeLinkList
     {
+        public KVNode Head { get; set; }
+        public KVNode Current { get; set; }
+
+        public KVNodeLinkList()
+        {
+            Head = null;
+        }
+
+        public void Insert(string key, string value)
+        {
+            KVNode node = new KVNode(key, value);
+            node.Next = Head;
+            Head = node;
+        }
+
+        public bool Includes(KVNode node)
+        {
+            Current = Head;
+            if (Head == null)
+            {
+                return false;
+            }
+            else
+            {
+                while (Current.Next != null)
+                {
+                    if (Current.Value == node.Value)
+                    {
+                        return true;
+                    }
+                    Current = Current.Next;
+                }
+                if (Current.Value == node.Value)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        public void Append(KVNode node)
+        {
+            Current = Head; 
+            while (Current.Next != null)
+            {
+                Current = Current.Next;
+            }
+            Current.Next = node;
+        }
     }
 }
