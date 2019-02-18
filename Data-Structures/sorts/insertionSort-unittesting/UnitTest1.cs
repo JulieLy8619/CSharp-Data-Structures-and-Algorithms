fb0a1e9137e3458c1c51b13eadadf18c67a3a5ec
juliely8619 <july8619@yahoo.com>
2019-02-17 17:15:35 -0800

    wrote insertion sort

--- Data-Structures/sorts/insertionSort-unittesting/UnitTest1.cs
+++ Data-Structures/sorts/insertionSort-unittesting/UnitTest1.cs
@@ -12,3 +12,12 @@ namespace insertionSort_unittesting
         }
     }
 }
+
+
+//A randomly generated unsorted array returns the array sorted
+//A sorted array returns the same sorted array
+//A backwards-sorted array returns the array sorted
+//An empty array returns the same empty array
+//An array of one element returns the same single-element array
+//add test for if have neg numbers
+//add test for if has duplicate
--- Data-Structures/sorts/insertionSort/Program.cs
+++ Data-Structures/sorts/insertionSort/Program.cs
@@ -6,7 +6,46 @@ namespace insertionSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //make an array of unsort int
+            int[] mainArray = new int[] {9, 8, 2, 1, 4};
+            Console.WriteLine("Array getting sent into insertion sort: ");
+            //for loop through array to print to screen
+            for (int i = 0; i < mainArray.Length; i++)
+            {
+                Console.Write(mainArray[i] + " ");
+            }
+            Console.WriteLine(); //just for spacing
+
+            //send it to the method
+            int[] answerArray = new int[mainArray.Length];
+            //it will return a sorted array
+            answerArray = InsertionSort(mainArray);
+            Console.WriteLine("Array results after insertion sort: ");
+            //for loop through array to print to screen
+            for (int i = 0; i < answerArray.Length; i++)
+            {
+                Console.Write(answerArray[i] + " ");
+            }
+            Console.WriteLine(); //just for spacing
+            Console.ReadLine(); // to stop it from auto exit
+        }
+
+        public static int[] InsertionSort(int[] arr)
+        {
+            //int[] returnArr = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int counter = i - 1;
+                int currentVal = arr[i];
+
+                while (counter >= 0 && currentVal < arr[counter])
+                {
+                    arr[counter + 1] = arr[counter];
+                    counter = counter - 1;
+                }
+                arr[counter + 1] = currentVal;
+            }
+            return arr;
         }
     }
 }
