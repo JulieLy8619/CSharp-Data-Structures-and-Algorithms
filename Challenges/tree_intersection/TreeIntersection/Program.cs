06110e145190e47da577b667bc7a9fd16a255ea4
juliely8619 <july8619@yahoo.com>
2019-02-20 09:40:03 -0800

    syncing what I have so I can turn in on time (started writing method, and rewriting some classes)

--- Challenges/tree_intersection/TreeIntersection/Program.cs
+++ Challenges/tree_intersection/TreeIntersection/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BreadthFirstTraversal.Classes;
+using HashTable.Classes;
+using System;
+using Tree.Classes;
 
 namespace TreeIntersection
 {
@@ -7,6 +10,15 @@ namespace TreeIntersection
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public int[] TreeIntersection(BinaryTree root1, BinaryTree root2)
+        {
+            QueueForTrees algoQueue = new QueueForTrees();
+            Hashtable algoHashtable = new Hashtable();
+            int[] returnArray = new int[10]; //how know size, do i need resize logic then?
+            int indexCounter = 0;
+            algoQueue.Enqueue(root1);
         }
     }
 }
