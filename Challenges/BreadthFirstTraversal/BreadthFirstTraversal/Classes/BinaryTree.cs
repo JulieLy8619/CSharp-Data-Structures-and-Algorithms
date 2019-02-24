d4531f97891c67b20a953adf6efc77d70f0058f0
juliely8619 <july8619@yahoo.com>
2019-02-23 16:10:15 -0800

    adeed binary tree to breathfirst because it has the queue and the nodes mostly already set up, just was missing a binary tree

---
+++ Challenges/BreadthFirstTraversal/BreadthFirstTraversal/Classes/BinaryTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreadthFirstTraversal.Classes
+{
+    class BinaryTree
+    {
+        public int counter = 0;
+        public TreeNode root { get; set; }
+
+        public BinaryTree(TreeNode rootNode)
+        {
+            root = rootNode;
+        }
+    }
+}
