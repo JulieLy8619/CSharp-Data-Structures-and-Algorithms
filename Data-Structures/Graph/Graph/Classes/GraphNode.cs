7cde02734e1501b3f69c740ad92dec7f39a17561
juliely8619 <july8619@yahoo.com>
2019-02-24 19:57:22 -0800

    added graph node

---
+++ Data-Structures/Graph/Graph/Classes/GraphNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph.Classes
+{
+    public class GraphNode
+    {
+        public string Value { get; set; }
+        public bool Visted { get; set; }
+        public GraphNode Next { get; set; }
+    }
+}
